a84091241a9171559dc1b90ab5a1ff98a874a5a3
madortill <madortill@gmail.com>
2023-07-04 11:25:13 +0300

    add github pages instructions to readme

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ This template should help get you started developing with Vue 3 in Vite.
 This is a boilerplate created by the command "npm init vue".
 
 **IMPORTANT! Before first upload - change <REPO_NAME> in `./vite.config.js` to your repository name.**  
+**Also, go to settings -> pages and change "deploy from branch" to Github "Actions"** 
+
 
 *Project Setup - before the first run:* run npm install  
 *run for development purposes:* npm run dev  
--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
-      <nav>
-      </nav>
-    </div>
-  </header>
+      </div>
 
 </template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
 
 <style scoped>
 header {

cd7a8a19b3528c1f1ce29963d58ee2099a0da7cc
madortill <mador.till@gmail.com>
2024-11-18 14:57:52 +0200

    hello

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      </div>
+      hello
+    </div>
 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <style scoped>
